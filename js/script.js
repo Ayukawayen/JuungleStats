94c7486558d658659c7f27675b0515503a500eb1
Ayukawayen <Ayukawayen@Gmail.com>
2021-04-02 11:07:26 +0800

    feat: rename to Juungle Portal

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ Vue.component('group-item', {
 	props: ['group'],
 	template: `
 		<a class="group" :href="group.groupId | href">
-			<div class="volume"><span class="label">Last 24hrs: </span>{{ group.volume | bch}} BCH</div>
+			<div class="volume"><span class="label">24h Volume: </span>{{ group.volume | bch}} BCH</div>
 			<div class="cnt">/ {{ group.cnt }} trades</div>
 			<div class="images">
 				<span class="image" v-for="item in group.items" :style="{ 'background-image': 'url(' + item.image + ')' }" :title="item.symbol">
